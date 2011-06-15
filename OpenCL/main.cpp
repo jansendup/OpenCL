c7ee00a30725be1b9b149f1f35eea72d41cd12a6
Jansen du Plessis <jansendup@gmail.com>
2011-06-16 00:32:19 +0200

    Up and running.

--- OpenCL/main.cpp
+++ OpenCL/main.cpp
@@ -6,7 +6,21 @@ int main(int argc, char** args)
 {
 	OCL ocl;
 	if ( !ocl.InitializeContext() )
+	{
 		printf("Failed to initialize cl device context\n");
+		goto END;
+	}
+	if( !ocl.LoadProgram("Test.cl") )
+	{
+		printf("Failed to Load Program.\n");
+		goto END;
+	}
+	if( !ocl.Run() )
+	{
+		printf("Failed to Run Program.\n");
+		goto END;
+	}
+END:
 	system("pause");
 	return 0;
 }
